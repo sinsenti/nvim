b270f15e0acd5d2ec0f86fd098dc4f27d9898058
sinsenti <sinsenti73@gmail.com>
2024-11-09 14:18:16 +0300

    add terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,34 +1,59 @@
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", opts)
--- buffers
-
-
+--
+--terminal
 vim.api.nvim_create_autocmd("TermOpen", {
     pattern = "*",
-    command = "startinsert", -- Automatically enter insert mode in the terminal
+    command = "startinsert",
 })
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", opts)
 vim.api.nvim_set_keymap("t", "jk", "<C-\\><C-n>", opts)
+
+function _G.open_split_terminal()
+    vim.cmd('vsplit')
+    local win = vim.api.nvim_get_current_win()
+    -- vim.wo[win].wrap = false  -- Disable line wrapping
+    vim.wo[win].number = false
+    vim.wo[win].relativenumber = false
+    vim.wo[win].cursorline = false  -- Disable cursorline
+    vim.wo[win].cursorcolumn = false  -- Disable cursorcolumn
+    local command = "ollama run llama3.2"
+    vim.cmd('terminal')
+    vim.api.nvim_put({command}, 'l', true, true)
+
+end
+vim.keymap.set('n', '<leader>wt', ':lua _G.open_split_terminal()<CR>', opts)
+
 function _G.open_floating_terminal()
-    local buf = vim.api.nvim_create_buf(false, true) -- Create a new empty buffer
+    local buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_open_win(buf, true, {
         relative = "editor",
-        width = 120,         -- Set the width of the floating terminal
-        height = 30,         -- Set the height of the floating terminal
-        col = math.floor((vim.o.columns - 120) / 2), -- Center the window horizontally
-        row = math.floor((vim.o.lines - 30) / 2),    -- Center the window vertically
-        style = 'minimal',   -- Use a minimal theme for the buffer
-        border = 'single',     -- Make the border transparent
-        -- background = '#f0f0f0' -- Set the background color to white
+        width = 120,
+        height = 30,
+        col = math.floor((vim.o.columns - 120) / 2),
+        row = math.floor((vim.o.lines - 30) / 2),
+        style = 'minimal',
+        border = 'single',
     })
     vim.cmd("terminal")
 end
+vim.keymap.set('n','wt',':lua _G.open_floating_terminal()<CR>', opts)
+vim.keymap.set('n', '<leader>dd', function()
+    vim.cmd('Format')
+    vim.cmd('w')
+    local path = vim.fn.expand('%:p')
+    local file_name = vim.fn.expand('%:t:r')
+    path = vim.fn.substitute(path, '\\', '/', 'g')
+    -- vim.fn.setreg('+', path)
+    -- local clipboard_content = vim.fn.getreg('+')
+    local command = "clang++ -o " .. file_name .. " " .. path .. " && ./" ..file_name..
+    vim.cmd('lua open_floating_terminal()')
+    vim.api.nvim_put({command}, 'l', true, true)
+end, opts)
 
-vim.api.nvim_set_keymap("n", "<leader>wt", "<cmd>lua open_floating_terminal()<CR>", opts)
-vim.keymap.set('n', 'wt', ':w<CR>:vsplit<CR>:terminal<CR>', opts)
 
 
+vim.api.nvim_set_keymap("n", "tw", ":Twilight<enter>", opts)
 vim.api.nvim_set_keymap("n", "tj", ":bfirst<CR>", opts)
 vim.api.nvim_set_keymap("n", "th", ":bprev<CR>", opts)
 vim.api.nvim_set_keymap("n", "tl", ":bnext<CR>", opts)
